708965edbd05602170ebae769a00f9b2709a8684
Ribka Chilaka <138965140+RibkaChilaka@users.noreply.github.com>
2024-01-09 11:39:22 +0530

    Update MyOrder.jsx

--- src/screens/MyOrder.jsx
+++ src/screens/MyOrder.jsx
@@ -6,7 +6,7 @@ function MyOrder() {
     const [orderData, setOrderData]=useState("")
     const fetchMyOrder = async () => {
         console.log(localStorage.getItem('userEmail'))
-        await fetch("http://localhost:5000/api/myOrderData", {
+        await fetch("https://fooddelivery-backend-rcha.onrender.com/api/myOrderData", {
             // credentials: 'include',
             // Origin:"http://localhost:3000/login",
             method: 'POST',
