15cf965362380369b4b736f3804299b82f6909b6
Ribka Chilaka <138965140+RibkaChilaka@users.noreply.github.com>
2024-01-09 11:38:20 +0530

    Update Home.jsx

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -11,7 +11,7 @@ export default function Home() {
   const [foodItem, setFoodItem] = useState([])
 
   const loadData = async () => {
-    let response = await fetch("http://localhost:5000/api/foodData", {
+    let response = await fetch("https://fooddelivery-backend-rcha.onrender.com//api/foodData", {
       method: "POST",
       header: {
         'Content-Type': 'application/json'
